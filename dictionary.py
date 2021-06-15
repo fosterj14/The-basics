0e007851992542373ab8e0af7374d6d31c405bc8
John Foster <johnfost.361@gmail.com>
2021-06-15 08:50:50 -0400

    updates

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+#similar to a list, but has entries assigned to identifiers
+student_grades = {"Mary": 9.1, "Sam": 8.8, "John": 7.5}
+
+print(student_grades.values) #gives just the values from the dictionary
+mysum = sum(student_grades.values)
---
+++ tuples.py
@@ -0,0 +1,3 @@
+temperatures = (1, 4, 5) #this is a tuple. note that it uses () and not [] like a list
+#tuples are immutable (const), cannot be changed at all
+#faster than lists
